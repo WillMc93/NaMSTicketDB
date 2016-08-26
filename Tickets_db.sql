ef96bc5aa5b718e2f97ec5d82d4da92622170b69
sirsleepy93 <sir.sleepy93@gmail.com>
2016-08-26 15:43:35 -0500

    Initial SQL Schema

---
+++ Tickets_db.sql
@@ -0,0 +1,37 @@
+DROP TABLE if exists technicians;
+DROP TABLE if exists tickets;
+DROP TABLE if exists added;
+DROP TABLE if exists completed;
+
+CREATE TABLE technicians (
+  TechID integer,
+  Name varchar(20) NOT NULL,
+  Admin boolean NOT NULL,
+  PRIMARY KEY (TechID)
+);
+
+CREATE TABLE tickets (
+  TicketID SERIAL,
+  Description text NOT NULL,
+  RoomNum varchar(4),
+  Reporter varchar(20) NOT NULL,
+  PRIMARY KEY (TicketID),
+);
+
+CREATE TABLE added (
+  TicketID integer,
+  AddedDate date NOT NULL,
+  AddedBy integer NOT NULL,
+  PRIMARY KEY (TicketID),
+  FOREIGN KEY (TicketID) REFERENCES tickets (TicketID),
+  FOREIGN KEY (AddedBy) REFERENCES technicians (TechID)
+);
+
+CREATE TABLE completed (
+  TicketID integer,
+  CompletedDate date NOT NULL,
+  CompletedBY integer NOT NULL,
+  PRIMARY KEY (TicketID),
+  FOREIGN KEY (TicketID) REFERENCES tickets (TicketID),
+  FOREIGN KEY (CompletedBy) REFERENCES technicians (TechID)
+);
