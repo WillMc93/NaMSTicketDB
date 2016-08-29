517c0608d24247af9ebe8bcd07f365ad74b2e151
sirsleepy93 <sir.sleepy93@gmail.com>
2016-08-29 17:00:46 -0500

    Alpha0.2
    Can Login. Can Add Tickets.

--- main.java
+++ main.java
@@ -1,11 +1,27 @@
 
+import java.util.Scanner;
+
 public class main {
 	public final static void main(String[] args) {
 		Connector connector = new Connector();
 		Technicians tech = new Technicians();
-		tech.login();
-	
-		System.out.println(tech.getUsername());
-	
-	}
+
+		Manage manage = null;
+
+		if (tech.login()) {
+			manage = new Manage();
+		}
+
+		Scanner in = new Scanner(System.in);
+
+		for (int i = 1; i > 0; --i) {
+
+
+			manage.addTicket("TEST", "015A", "Jerome");
+			manage.modTicket(1, "TESTTESTTEST");
+			manage.completeTicket(1);
+
+		}
+	}	
 }
+
