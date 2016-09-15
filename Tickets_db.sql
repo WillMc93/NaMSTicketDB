5790e0d394e284646336c412fae09ca685a4d7c1
sirsleepy93 <sir.sleepy93@gmail.com>
2016-09-15 16:46:55 -0500

    Twiddled Thumbs

--- Tickets_db.sql
+++ Tickets_db.sql
@@ -1,21 +1,25 @@
+DROP TABLE if exists activeTechnicians;
+DROP TABLE if exists completed;
+DROP TABLE if exists added;
 DROP TABLE if exists technicians;
 DROP TABLE if exists tickets;
-DROP TABLE if exists added;
-DROP TABLE if exists completed;
+
 
 CREATE TABLE technicians (
-  TechID integer,
+  TechID SERIAL,
   Name varchar(20) NOT NULL,
   Admin boolean NOT NULL,
+  Employed boolean NOT NULL,
   PRIMARY KEY (TechID)
 );
+
 
 CREATE TABLE tickets (
   TicketID SERIAL,
   Description text NOT NULL,
   RoomNum varchar(4),
   Reporter varchar(20) NOT NULL,
-  PRIMARY KEY (TicketID),
+  PRIMARY KEY (TicketID)
 );
 
 CREATE TABLE added (
@@ -35,3 +39,5 @@ CREATE TABLE completed (
   FOREIGN KEY (TicketID) REFERENCES tickets (TicketID),
   FOREIGN KEY (CompletedBy) REFERENCES technicians (TechID)
 );
+
+INSERT INTO technicians (Name, Admin, Employed) VALUES ('postgres', TRUE, TRUE);
